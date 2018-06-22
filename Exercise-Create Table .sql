4c6671afdf1c424f8af82a6db084731b54f5ef69
Benedictus Aryo <benedictus.aryo@myoffice365.site>
2018-06-22 14:40:32 +0700

    Exercise Foregin key

--- Exercise-Create Table .sql
+++ Exercise-Create Table .sql
@@ -60,5 +60,66 @@ primary key (purchase_number)
 );
 
 
+-- Exercise Primary key constraint :
+drop table customers;
+
+-- Exercise Create Table Constraint Primary Key
+create table customers
+(
+ customer_id int,
+ first_name varchar(255),
+ last_name varchar(255),
+ email_address varchar(255),
+ number_of_complaints int,
+primary key(customer_id)
+);
+
+drop table if exists items;
+
+create table items
+(
+ item_id varchar(255),
+ item varchar(255),
+ unit_price numeric(10,2),
+ company_id varchar(255),
+primary key(item_id)
+ );
+  
+drop table if exists companies;
+
+create table companies
+(
+ company_id varchar(255),
+ company_name varchar(255),
+ headquarter_phone_number int(12),
+primary key(company_id)
+ );
+ 
+ /*
+ Exercise FOREIGN KEY Constraint
+ 
+ */
+ 
+ -- FOREIGN KEY Constraint 
+
+drop table sales;
+
+CREATE TABLE sales
+(
+ purchase_number INT NOT NULL auto_increment,
+ date_of_purchase DATE NOT NULL,
+ customer_id INT,
+ item_code VARCHAR(10) NOT NULL,
+primary key (purchase_number),
+foreign key (customer_id) references customers(customer_id) on delete cascade
+);
+
+-- Cara kedua :
+
+alter table sales
+add foreign key (customer_id) references customers(customer_id) on delete cascade;
+
+
+
 
 
