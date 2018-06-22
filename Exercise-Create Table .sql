2963216cdcbfc966bb32ffeb638e7fd76469b212
Benedictus Aryo <benedictus.aryo@myoffice365.site>
2018-06-22 10:39:54 +0700

    Exercise pertama Create Table

---
+++ Exercise-Create Table .sql
@@ -0,0 +1,30 @@
+# Create Database sales
+create database	if not exists sales;
+# Select sales database as a working area
+use sales;
+
+# Create table sales Tutorial :
+
+CREATE TABLE sales
+(
+purchase_number INT NOT NULL PRIMARY KEY auto_increment,
+date_of_purchase DATE NOT NULL,
+customer_id INT,
+item_code VARCHAR(10) NOT NULL
+);
+
+# Create Table exercise
+
+CREATE TABLE customers
+(
+customer_id INT NOT NULL PRIMARY KEY auto_increment,
+first_name VARCHAR(255),
+last_name VARCHAR(255),
+email_address VARCHAR(255),
+number_of_complaints INT
+);
+
+
+
+
+
